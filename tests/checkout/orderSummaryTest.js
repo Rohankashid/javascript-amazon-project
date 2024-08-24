698a029d758172050d1f1693591096f5b3147e43
Rohankashid <rohankashid25753@gmail.com>
2024-08-25 00:16:04 +0530

    load products in beforeAll hook

--- tests/checkout/orderSummaryTest.js
+++ tests/checkout/orderSummaryTest.js
@@ -1,18 +1,30 @@
 import { loadFromStorage , cart } from "../../data/cart.js";
 import { deliveryOptions } from "../../data/deliveryOptions.js";
 import { renderOrderSummary } from "../../scripts/checkout/orderSummary.js";
+import { loadProducts } from "../../data/products.js";
 
 
 describe('test suit : renderOrderSummary' ,()=>{
-    it('removes a product  ', ()=>{
-        document.querySelector('.js-test-container').innerHTML = `
-        <div class = "js-order -summary"></div>
-        <div class = "js-payment-summary"></div>
-        
-        `;
+    
+    
 
         const productId1 = 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6';
         const productId2 = '15b6fc6f-327a-4ec4-896f-486349e85a3d';
+
+        beforeAll((done)=>{
+            loadProducts(()=>{
+                done();
+            });
+        });
+
+        beforeEach(()=>{
+            spyOn(localStorage , 'setItem')
+        })
+
+        document.querySelector('.js-test-container').innerHTML = `
+        <div class = "js-order-summary"></div>
+        <div class = "js-payment-summary"></div>
+        `
 
         spyOn(localStorage , 'getItem').and.callFake(()=>{
             return JSON.stringify([{
@@ -80,5 +92,5 @@ describe('test suit : renderOrderSummary' ,()=>{
 
             expect(cart.length).toEqual(1);
             expect(cart[0].productId).toEqual(productId2)
-    })
-})
+    }
+)
